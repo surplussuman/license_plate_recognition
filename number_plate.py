af5490fc90d620896eb430fe2e21796971393fbf
Suman <92079502+surplussuman@users.noreply.github.com>
2024-03-30 19:31:52 +0530

    first commit

---
+++ number_plate.py
@@ -0,0 +1,56 @@
+import cv2
+import easyocr
+import pandas as pd
+harcascade = "model/haarcascade_russian_plate_number.xml"
+
+cap = cv2.VideoCapture(1)
+
+cap.set(3, 640) # width
+cap.set(4, 480) #height
+
+reader = easyocr.Reader(['en'])
+data = []
+
+min_area = 500
+count = 0
+
+while True:
+    success, img = cap.read()
+
+    plate_cascade = cv2.CascadeClassifier(harcascade)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    plates = plate_cascade.detectMultiScale(img_gray, 1.1, 4)
+
+    for (x,y,w,h) in plates:
+        area = w * h
+
+        if area > min_area:
+            cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+            cv2.putText(img, "Number Plate", (x,y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2)
+
+            img_roi = img[y: y+h, x:x+w]
+            cv2.imshow("ROI", img_roi)
+
+
+    
+    cv2.imshow("Result", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("plates/scaned_img_" + str(count) + ".jpg", img_roi)
+        cv2.rectangle(img, (0,200), (640,300), (0,255,0), cv2.FILLED)
+        cv2.putText(img, "Plate Saved", (150, 265), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255), 2)
+        cv2.imshow("Results",img)
+        cv2.waitKey(500)
+ # OCR
+        output = reader.readtext("plates/scaned_img_" + str(count) + ".jpg")
+        if output:
+            plate_number = output[0][-2]
+            data.append({"Plate Number": plate_number})
+            df = pd.DataFrame(data)
+            df.to_excel("plate_numbers.xlsx", index=False)
+            print("Plate Number:", plate_number)
+                
+        count += 1
+        
+
